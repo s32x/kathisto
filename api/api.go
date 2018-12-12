5bfc2fb4e3662209b8746581dd0183721b41d41f
Steven Wolfe <steven@swolfe.me>
2018-12-11 21:30:19 -0700

    Go modules

--- api/api.go
+++ api/api.go
@@ -13,8 +13,7 @@ func Start(version, userAgent, pubDir, host, port string) {
 	log.Printf("kathisto v%s - Server-Side rendering with Go/Headless Chrome\n", version)
 
 	// Create a Chrome Renderer and bind the Render func to /
-	r := renderer.NewChromeRenderer(userAgent)
-	rs := service.NewService(r, host, pubDir)
+	rs := service.NewService(renderer.NewChromeRenderer(userAgent), host, pubDir)
 	http.HandleFunc("/", rs.Render)
 
 	// Always run a basic http server
--- renderer/chrome.go
+++ renderer/chrome.go
@@ -6,34 +6,32 @@ import (
 	"os/exec"
 )
 
-// noRenderKey is used in the queryparam of an forced
-// un-rendered request to avoid recursive rendering
+// noRenderKey is used in the queryparam of an forced un-rendered request to
+// avoid recursive rendering
 const noRenderKey = "norender"
 
-// Chrome is a struct containing all dependencies needed
-// to perform HTML rendering and response caching using
-// Headless Chrome. It satisfies the Renderer interface
+// Chrome is a struct containing all dependencies needed to perform HTML
+// rendering and response caching using Headless Chrome. It satisfies the
+// Renderer interface
 type Chrome struct {
 	userAgent string
 	cache     map[string][]byte
 }
 
-// NewChromeRenderer generates a new Chrome Renderer that
-// renders and stores rendered pages in a cache
+// NewChromeRenderer generates a new Chrome Renderer that renders and stores
+// rendered pages in a cache
 func NewChromeRenderer(userAgent string) Renderer {
 	return &Chrome{userAgent, make(map[string][]byte)}
 }
 
-// Render renders a page using Headless Chrome and returns
-// the bytes
+// Render renders a page using Headless Chrome and returns the bytes
 func (c *Chrome) Render(url string) ([]byte, error) {
 	// Return any previously rendered cached bytes
 	if c.cache[url] != nil {
 		return c.cache[url], nil
 	}
 
-	// Construct a norender url to avoid recursive
-	// rendering
+	// Construct a norender url to avoid recursive rendering
 	url = fmt.Sprintf("%s?%s=true", url, noRenderKey)
 
 	// Execute the render command using the norender url
@@ -54,8 +52,7 @@ func (c *Chrome) Render(url string) ([]byte, error) {
 	return page, nil
 }
 
-// stripErrors strips all prefixing errors from the
-// rendered webpage
+// stripErrors strips all prefixing errors from the rendered webpage
 func stripErrors(body []byte) []byte {
 	buf := bytes.NewBuffer(body)
 	for {
